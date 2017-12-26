15fb982626d70b80117896c7dc815b0dac93c507
Arham Jamaluddin Siddiqui <arhamjsiddiqui@gmail.com>
2017-12-26 17:39:16 -0600

    implementation of the sendCountdown inline function in Java

--- app/src/main/java/sszg/com/lesson1/Test.java
+++ app/src/main/java/sszg/com/lesson1/Test.java
@@ -1,5 +1,7 @@
 package sszg.com.lesson1;
 
+import android.os.CountDownTimer;
+
 import kotlin.Unit;
 import kotlin.jvm.functions.Function0;
 
@@ -9,12 +11,29 @@ import kotlin.jvm.functions.Function0;
 
 public class Test {
     public static void main(String[] args) {
-        MainActivity.Companion.sendCountdown(50, 50, new Function0<Unit>() {
+        // can alternately call MainActivity.Companion.sendCountdown() for static call. We need a
+        // companion object {} for static in Kotlin, it's sort of annoying.
+        sendCountdown(50, 50, new Function0<Unit>() {
             @Override
             public Unit invoke() {
                 // put code here
                 return null;
             }
         });
+    }
+
+    /**
+     * Here's an implementation of the sendCountdown inline function in Java.
+     */
+    private static void sendCountdown(long millisInFuture, long countDownInterval, final Function0<Unit> function0) {
+        new CountDownTimer(millisInFuture, countDownInterval) {
+            @Override
+            public void onTick(long l) {}
+
+            @Override
+            public void onFinish() {
+                function0.invoke();
+            }
+        }.start();
     }
 }
